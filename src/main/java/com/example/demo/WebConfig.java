7d6720f40f636245b7a4630822d9aaefb361a661
Levi Liester <levi.liester@gmail.com>
2022-03-03 00:40:27 -0600

    Back to previous

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://levi.com");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
